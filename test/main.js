20f24dae01f6b583b1e6d85fda4bb442bfa93584
Roman Pominov <rpominov@gmail.com>
2016-03-28 18:43:58 +0300

    use lobot dev tool

--- test/main.js
+++ test/main.js
@@ -1,20 +1,14 @@
-import test from 'tape-catch'
+import test from 'lobot/test'
 import Signal from '../src'
 
-test('of', t => {
-  t.plan(1)
+test('of', 1, t => {
   t.equal(Signal.of(1).get(), 1)
-  t.end()
 })
 
-test('of+map', t => {
-  t.plan(1)
+test('of+map', 1, t => {
   t.equal(Signal.map(x => x + 1, Signal.of(1)).get(), 2)
-  t.end()
 })
 
-test('of+ap', t => {
-  t.plan(1)
+test('of+ap', 1, t => {
   t.equal(Signal.ap(Signal.of(x => x + 1), Signal.of(1)).get(), 2)
-  t.end()
 })
